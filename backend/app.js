9256c74987acddf8c393b5b6d69aeee9842c5787
Christin Parker <cparker4400@gmail.com>
2024-10-05 18:37:08 -0500

    completed milestone 1

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,48 @@
 const express = require('express');
 const mongoose = require('mongoose')
-
+const userController = require('./controllers/user')
 const app = express();
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use('/api/auth', (req, res, next) => {
+  const stuff = [
+    {
+      email: 'test@test.com',
+      password: 'qeesgtrj',
+    },
+  ];
+  res.status(200).json(stuff);
+});
+
+app.use('/api/sauces', (req, res, next) => {
+  const stuff = [
+    {
+      _id: 'oeihfzeoi',
+      title: 'My first thing',
+      description: 'All of the info about my first thing',
+      imageUrl: '',
+      price: 4900,
+      userId: 'qsomihvqios',
+    },
+    {
+      _id: 'oeihfzeomoihi',
+      title: 'My second thing',
+      description: 'All of the info about my second thing',
+      imageUrl: '',
+      price: 2900,
+      userId: 'qsomihvqios',
+    },
+  ];
+  res.status(200).json(stuff);
+});
 
 // const user = require('./models/user');
 
@@ -34,12 +75,7 @@ mongoose.connect('mongodb+srv://cparker4400:1SseRxj3NnINk6ll@cluster0.x1erk.mong
     console.error(error);
   });
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  next();
-});
+
 
 
 // app.use((req, res, next) => {
@@ -60,5 +96,6 @@ app.use((req, res, next) => {
 // app.use((req, res, next) => {
 //     console.log('Response sent successfully!');
 // });
+app.use('/api/auth',userController.signup)
 
 module.exports = app;
